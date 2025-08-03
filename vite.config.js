2a4763ab9254e594705f0ae8e84e860ebfdea028
Ramadan <fitrahikaya@gmail.com>
2025-08-03 22:35:02 +0800

    siap

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    build: {
+        outDir: "public/build", // hasil build akan masuk sini
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.jsx"],
